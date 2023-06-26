97a0483704086383913d6d5f872ad665ebbe19f0
Laureano Serrano <serranolaureano.220@gmail.com>
2023-06-26 08:44:57 -0300

    added status bar component

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar
+        style="auto"
+        translucent={true}
+        backgroundColor="transparent"
+      />
       <Navigation />
     </NavigationContainer>
   );
